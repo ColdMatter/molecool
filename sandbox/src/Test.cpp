8757034f8b195ba2563a424429c726e19571eb3e
Noah Fitch <nofitch@gmail.com>
2020-06-03 17:26:58 +0100

    Connects 'sandbox' testbed application to static 'molecool' library.

--- sandbox/src/Test.cpp
+++ sandbox/src/Test.cpp
@@ -1,26 +1,42 @@
 #define BOOST_TEST_MODULE mytests
 #include <boost/test/included/unit_test.hpp>
 #include <mkl.h>
+#include <array>
+#include "test.h"
 
 
 BOOST_AUTO_TEST_CASE(myTestCase)
 {
+
+  molecool::Print();
+
   BOOST_TEST(1 == 1);
   BOOST_TEST(true);
 
+  ///////////////////////////////////////////////
+  // quick test of MKL random number generation
   int seedy = (int)time(0);							// current time in seconds as a seed
   VSLStreamStatePtr stream;							// stream for random number generation
   vslNewStream(&stream, VSL_BRNG_MCG31, seedy); 	// stream, generator type, seed 
-
-  // random number generation
   const int METHOD = 0;
   const int nValues = 10;
-  double randomArrayTarget[nValues];
+  std::array<double, nValues> randomArrayTarget;
   double mean = 0;
   double width = 1;
-  double d = vdRngGaussian(METHOD, stream, nValues, randomArrayTarget, mean, width);
+  double d = vdRngGaussian(METHOD, stream, nValues, randomArrayTarget.data(), mean, width);
   
+  // print the array the old-fashioned way
   for (int i = 0; i < nValues; ++i) {
-	  printf("value %d = %f\n", i, randomArrayTarget[i]);
+	  printf("value %d = %f\n", i, randomArrayTarget.at(i));
   }
+
+  // or, printing the C++11 way using a range-based for loop
+  // actually awkward to extract the current index in this case
+  for (const auto& rand : randomArrayTarget) {
+	  printf("value %I64u = %f\n", &rand - &(randomArrayTarget[0]), rand);
+  }
+
+  ///////////////////////////////////////////////
+  // quick test of Boost random number generation
+
 }
