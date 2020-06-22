3dd8b65b670ac4e08d4f6454229bd066eef54416
Noah Fitch <nofitch@gmail.com>
2020-06-22 06:42:39 +0100

    Minor edits before trying to implement a build system.

--- sandbox/src/SandboxApp.cpp
+++ sandbox/src/SandboxApp.cpp
@@ -24,20 +24,20 @@ namespace molecool {
 	// Basically, DEFINE YOUR SIMULATION HERE
 	Simulation* createSimulation() {
 
-
 		//-------------------------------------
 		// A quick test of ensemble creation and initialization
-		// construct desired molecule distributions for {x, y, z, vx, vy, vz}
+		// construct desired molecule distributions
 		std::array<Distribution, 6> distributions = {
-			Distribution(Shape::gaussian, 0, 0.1),
-			Distribution(Shape::gaussian, 1, 0.1),
-			Distribution(Shape::gaussian, 2, 0.1),
-			Distribution(Shape::gaussian, 3, 0.1),
-			Distribution(Shape::gaussian, 4, 0.1),
-			Distribution(Shape::gaussian, 5, 0.1)
+			Distribution(Shape::gaussian, 0, 0.1),	// x
+			Distribution(Shape::gaussian, 1, 0.1),	// y
+			Distribution(Shape::gaussian, 2, 0.1),	// z
+			Distribution(Shape::gaussian, 3, 0.1),	// vx
+			Distribution(Shape::gaussian, 4, 0.1),	// vy
+			Distribution(Shape::gaussian, 5, 0.1)	// vz
 		};
 		// generate the ensemble of particles and initialize them using the given distributions
-		long nParticles = 10'000'000;	// 1e7 particles occupy about 500MB of heap memory
+		// 1e7 particles occupy ~500MB of heap memory (peak usage during construction is double that)
+		long nParticles = 10'000'000;	
 		Ensemble ensemble(nParticles, distributions);
 		//-------------------------------------
 
