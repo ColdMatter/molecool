d2501f626478b97a152305776bf60e9c161b0d4e
Noah Fitch <nofitch@gmail.com>
2020-06-28 08:38:01 +0100

    Update premake script to use environmental variables for MKL and Boost installation directories and make project setup more platform independent.  Requires the contributing developer to define two environment variables, MKLROOT and BOOSTROOT, the rest is automatic.

--- premake5.lua
+++ premake5.lua
@@ -10,10 +10,19 @@ workspace "molecool" -- workspace/solution name
     outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
     -- e.g. \Debug-windows-x86_64
 
+    -- include paths and linking depend on the following environment variables
+    mklRootDir = os.getenv("MKLROOT")   -- e.g. "C:/Program Files (x86)/IntelSWTools/compilers_and_libraries_2020.1.216/windows/"
+    boostDir = os.getenv("BOOSTROOT")   -- e.g. "C:/Boost/boost_1_73_0/"
+
+    -- setup the mkl architecture for path construction
+    mklArch = "intel64"
     filter "system:windows"
-        mklRootDir = "C:/Program Files (x86)/IntelSWTools/compilers_and_libraries_2020.1.216/windows/"
-        mklLibDir = mklRootDir .. "mkl/lib/intel64_win/"
-        boostDir = "C:/Boost/boost_1_73_0"
+        mklArch = mklArch .. "_win"
+    filter {}   -- deactivate filter
+
+    mklLibDir = mklRootDir .. "mkl/lib/" .. mklArch .. "/"
+    mklOmpDir = mklRootDir .. "compiler/lib/" .. mklArch .. "/"
+    mklIncDir = mklRootDir .. "mkl/include"
 
 
 project "molecool"
@@ -27,6 +36,9 @@ project "molecool"
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("build/" .. outputdir .. "/%{prj.name}")
 
+    pchheader "mcpch.h"
+    pchsource "molecool/src/mcpch.cpp" -- VS only, ignored on other compilers
+
     files -- files to include in project
     {
         "%{prj.name}/src/**.h",
@@ -35,26 +47,28 @@ project "molecool"
 
     includedirs -- compiler include directories
     {
+        "%{prj.name}/src",
         "%{prj.name}/vendor/spdlog/include",
         boostDir,
-        mklRootDir .. "mkl/include"
+        mklIncDir
     }
 
-    libdirs
+    libdirs -- library search paths for the linker
     {
-        mklLibDir
+        mklLibDir,
+        mklOmpDir
     }
 
-    links 
+    links -- libraries/projects to link against, like VS's "references" 
     {
-        mklLibDir .. "mkl_intel_ilp64.lib",     -- interface
-        mklLibDir .. "mkl_intel_thread.lib",    -- threading
-        mklLibDir .. "mkl_core.lib",            -- core
+        "mkl_intel_ilp64.lib",     -- interface
+        "mkl_intel_thread.lib",    -- threading
+        "mkl_core.lib",            -- core
+        "libiomp5md.lib"           -- openmp
     }
 
     filter "system:windows"
         
-
         defines 
         {
             
@@ -64,11 +78,6 @@ project "molecool"
         {
             "/openmp",
             "/DMKL_ILP64"
-        }
-
-        links 
-        {
-            mklRootDir .. "compiler/lib/intel64_win/libiomp5md.lib"     -- openmp
         }
 
     filter "configurations:Debug"
@@ -91,31 +100,33 @@ project "sandbox"
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("build/" .. outputdir .. "/%{prj.name}")
 
-    files -- files to include in project
+    files
     {
         "%{prj.name}/src/**.h",
         "%{prj.name}/src/**.cpp"
     }
 
-    includedirs -- compiler include directories
+    includedirs
     {
-        "molecool/vendor/spdlog/include",
         "molecool/src",
+        "molecool/vendor/spdlog/include",
         boostDir,
-        mklRootDir .. "mkl/include"
+        mklIncDir
     }
 
     libdirs
     {
-        mklLibDir
+        mklLibDir,
+        mklOmpDir
     }
 
     links 
     {
         "molecool",
-        mklLibDir .. "mkl_intel_ilp64.lib",     -- interface
-        mklLibDir .. "mkl_intel_thread.lib",    -- threading
-        mklLibDir .. "mkl_core.lib",            -- core
+        "mkl_intel_ilp64.lib",    
+        "mkl_intel_thread.lib",    
+        "mkl_core.lib",   
+        "libiomp5md.lib"
     }
 
     filter "system:windows"
@@ -129,11 +140,6 @@ project "sandbox"
         {
             "/openmp",
             "/DMKL_ILP64"
-        }
-
-        links 
-        {
-            mklRootDir .. "compiler/lib/intel64_win/libiomp5md.lib"     -- openmp
         }
 
     filter "configurations:Debug"
