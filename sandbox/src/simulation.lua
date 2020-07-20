504874278a5f48bb40c410a47e048bf7590417ca
Noah Fitch <nofitch@gmail.com>
2020-07-20 12:31:16 +0100

    Setting up for embedding Lua.  The main script will reside in the user application src/ directory so I've reverted back to using the project directory as the working directory.  To facilitate the current routing of output files, this has required making a tracked output/ directory as part of the sandbox project.

---
+++ sandbox/src/simulation.lua
@@ -0,0 +1,13 @@
+simulation = {
+    position = {
+    x = 32.5, y = 20.0
+    },
+    filename = "sprite.png",
+    begin   = 0.0
+    finish  = 1.0
+    dt      = 0.001
+}
+
+number = 10
+
+array = {1, 1, 2, 3, 5, 10, 20}
