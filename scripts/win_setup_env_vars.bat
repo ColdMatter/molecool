3fb451379c13e4405d9c635dc7377bd130e7f178
fitchn <nfitch@ic.ac.uk>
2020-09-01 16:11:59 +0100

    Fixes directories for windows environment variables

--- scripts/win_setup_env_vars.bat
+++ scripts/win_setup_env_vars.bat
@@ -1,3 +1,3 @@
-setx MKLROOT "C:/Program Files (x86)/IntelSWTools/compilers_and_libraries_2020.1.216/windows/" /m
-setx BOOSTROOT "C:/Boost/Boost_1_73_0/" /m
+setx MKLROOT "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2020.2.254\windows\"
+setx BOOSTROOT "C:\Boost\Boost_1_74_0\" 
 pause
